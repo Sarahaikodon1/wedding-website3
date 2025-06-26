7b9649a28e706815f03bf12a28f0b38c678c2928
Sarahaikodon1 <okoromi.beauty@gmail.com>
2025-06-25 22:23:20 -0400

    fix blank screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import WeddingWebsite from './WeddingWebsite';
 
 function App() {
-  return <WeddingWebsite />;
+  return (
+    <div>
+      <h1>Welcome to Our Wedding!</h1>
+      <p>Glory & Femi — February 2026</p>
+    </div>
+  );
 }
 
 export default App;
